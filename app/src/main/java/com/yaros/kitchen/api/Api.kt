9aa7a6fd0d53fbe1545a74c2280000af3532d743
Akgun Studio <hasanyildiz5151@gmail.com>
2020-01-22 16:05:37 +0600

    added credentials

--- app/src/main/java/com/yaros/kitchen/api/Api.kt
+++ app/src/main/java/com/yaros/kitchen/api/Api.kt
@@ -1,4 +1,22 @@
 package com.yaros.kitchen.api
 
+import okhttp3.Interceptor
+
 class Api {
+
+
+    private val headerAuthInterceptor =
+        label@ Interceptor { chain: Interceptor.Chain ->
+            // try {
+                chain.call().cancel()
+                val original = chain.request()
+                chain.proceed(original).close()
+                return@label chain.proceed(original)
+            val requestBuilder = original.newBuilder()
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .header("Authorization", "Bearer ${AUTH_PASS}")
+            val request = requestBuilder
+                .build()
+            chain.proceed(request)
+        }
 }
